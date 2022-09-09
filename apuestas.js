413b878dab1e0a0121ec24c95ee2072f36bc1754
Santiago Castagnaviz <castagnaviz.santiago@gmail.com>
2022-09-09 10:52:06 -0300

    2

--- apuestas.js
+++ apuestas.js
@@ -1,21 +1,32 @@
-
-
-
-
 function jugar() {
-    let number = document.getElementById('number');
-    if (number > 12 || number < 0) return alert("Numero invalido");
+    let number = document.getElementById("dice").value;
 
-    dice1 = Math.floor((Math.random() * 5) + 1)
-    dice2 = Math.floor((Math.random() * 5) + 1)
+
+    let bet = document.getElementById("apuesta").value;
+    let earn = bet;
+    if (number > 12 || number < 0) alert("Numero invalido");
+
+    let dice1 = Math.floor(Math.random() * 5 + 1);
+    let dice2 = Math.floor(Math.random() * 5 + 1);
 
     alert(`Dado 1: ${dice1}  \nDado 2: ${dice2} \nLa suma es ${dice1 + dice2}`);
 
-    let winOrLose = false
-    if (number == dice1 + dice2) winOrLose = true;
-    else if (number == dice1 || number == dice2) return (winOrLose = true)
+    let winOrLose = false;
 
-    if (winOrLose) alert("Apuesta ganadora");
-    else alert("Apuesta perdedora");
+    if (number == (dice1 + dice2)) {
+        winOrLose = true;
+         earn =  bet * 3;
+    } 
+    if (number == dice1 || number == dice2) {
+        winOrLose = true;
+        if (dice2 == dice1) {
+         earn = bet * 3;
+        } else {
+        earn = bet * 2;
+        }
+    }
+
+    if (winOrLose == true) { alert(`Apuesta ganadora! \nGano: ${earn} `); }
+    if (winOrLose == false) { alert(`Apuesta perdedora \nGano:$ ${earn}\nPerdio $: ${bet}`); }
+
 }
-//console.log( jugar(15))
